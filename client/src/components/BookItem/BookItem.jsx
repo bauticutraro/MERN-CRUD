397257b7e221ce91f92f53260f182a0285787d24
German Cutraro <germancutraro@hotmail.com>
2020-02-03 20:06:48 -0300

    book item clean up

--- client/src/components/BookItem/BookItem.jsx
+++ client/src/components/BookItem/BookItem.jsx
@@ -1,86 +1,38 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { amber } from '@material-ui/core/colors';
-import { useDispatch } from 'react-redux';
-
+import { useHistory } from 'react-router-dom';
 import { Delete, Edit } from '@material-ui/icons';
+// redux
+import { useDispatch } from 'react-redux';
 import { deleteBookStart } from '../../containers/Books/booksActions';
 
-const useStyles = makeStyles(theme => ({
-  card: {
-    maxWidth: 345,
-    marginBottom: 20
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%' // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest
-    })
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)'
-  },
-  avatar: {
-    backgroundColor: amber[500]
-  }
-}));
-
-function BookItem(props) {
-  const classes = useStyles();
-
+const BookItem = ({ _id, title, price, description, file: image }) => {
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const handleDeleteBook = id => dispatch(deleteBookStart(id));
+  const handleChangeRoute = id => history.push(`/edit/${id}`);
 
   return (
-    <Card className={classes.card}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label='recipe' className={classes.avatar}>
-            {props.title[0].toUpperCase()}
-          </Avatar>
-        }
-        title={props.title}
-      />
-      <CardMedia
-        className={classes.media}
-        image={props.file}
-        title='Paella dish'
-      />
-      <CardContent>
-        <Typography variant='body2' color='textSecondary' component='p'>
-          {props.description}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton
-          aria-label='Edit'
-          onClick={() => props.handleChangeRoute(`/edit/${props._id}`)}
-        >
-          <Edit />
-        </IconButton>
-        <IconButton
-          aria-label='Delete'
-          onClick={() => handleDeleteBook(props._id)}
-        >
-          <Delete />
-        </IconButton>
-      </CardActions>
-    </Card>
+    <div>
+      <h3>{title}</h3>
+      <div>
+        <img
+          src={
+            image ||
+            'https://clipartart.com/images/a-lot-of-books-clipart-6.png'
+          }
+          width={150}
+          height={170}
+        />
+      </div>
+      <span>{description}</span>
+      <h4>${price}</h4>
+      <div>
+        <Edit onClick={() => handleChangeRoute(_id)} />
+        <Delete onClick={() => handleDeleteBook(_id)} />
+      </div>
+    </div>
   );
-}
+};
 
 export default React.memo(BookItem);
