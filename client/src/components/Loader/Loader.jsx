398f305b10730b139fbd6fda7f5abb2299ee9560
German Cutraro <germancutraro@hotmail.com>
2020-02-03 19:56:15 -0300

    loader component pure css

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,14 +1,57 @@
 import React from 'react';
-import { Grid, Container, CircularProgress } from '@material-ui/core';
+import styled from 'styled-components';
 
-const Loader = () => {
-  return (
-    <Container fixed>
-      <Grid container direction='row' justify='center' alignItems='center'>
-        <CircularProgress />
-      </Grid>
-    </Container>
-  );
-};
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledSpinner = styled.svg`
+  animation: rotate 2s linear infinite;
+  margin: -25px 0 0 -25px;
+  width: 50px;
+  height: 50px;
+  & .path {
+    stroke: #43425d;
+    stroke-linecap: round;
+    animation: dash 1.5s ease-in-out infinite;
+  }
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 150;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -35;
+    }
+    100% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -124;
+    }
+  }
+`;
+
+const Loader = () => (
+  <Container>
+    <StyledSpinner viewBox='0 0 50 50'>
+      <circle
+        className='path'
+        cx='25'
+        cy='25'
+        r='20'
+        fill='none'
+        strokeWidth='4'
+      />
+    </StyledSpinner>
+  </Container>
+);
 
 export default Loader;
